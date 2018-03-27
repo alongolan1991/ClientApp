1e5f7625635efc9df85fcf73ec03fe65787cfd2e
Alon Golan <eggnoob@gmail.com>
2018-03-27 14:04:22 +0300

    fix file

--- includes/main.js
+++ includes/main.js
@@ -28,7 +28,7 @@
 
     var myData = 'func=' + action + "&num1=" + num1 + "&num2=" + num2 + "&num3=" + num3;
     $.ajax({
-      url: "http://localhost/service_calculator/dcs4.php",
+      url: "http://shenkar.html5-book.co.il/2017-2018/dcs/dev_35/service_calculator/dcs4.php",
       datatype:'json',
       type: method1,
       data:myData,
